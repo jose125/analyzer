4e17e9d79a13b8af49abc9840260712eb35dd6eb
Jose Gomez <j.gbaco.12@gmail.com>
2018-04-07 16:39:06 +0000

    aplicacion base

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('information/', include('information.urls')),
     path('admin/', admin.site.urls),
+    path('', include('information.urls')),
 ]
